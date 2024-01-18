29d8e09c844932260c8cfb9dc209c98757641819
Bakabeko <bakamoaz75@gmail.com>
2024-01-19 00:48:39 +0200

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 # 2-args.py
-# abubkr moaz
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-
     """Print the number of and list of arguments."""
-
     import sys
-    count = len(sys.argv)
+
+    count = len(sys.argv) - 1
     if count == 0:
-        print("{} arguments.", sys.len(argv))
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments: ".format(count - 1))
-        for i in range(1, count):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,7 +3,7 @@
 # abubkr moaz
 
 if __name__ == "__main__":
-        """Print the value of variable a from variable_load_5."""
+    """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
     print(a)
