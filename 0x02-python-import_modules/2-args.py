b19b1b4a98c1ca8aa80299a076affe800b91b196
Bakabeko <bakamoaz75@gmail.com>
2024-01-19 00:09:51 +0200

    interristing

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 2-args.py
+# abubkr moaz
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+    count = len(sys.argv)
+    if count == 0:
+        print("{} arguments.", sys.len(argv))
+    else:
+        print("{} arguments: ".format(count - 1))
+        for i in range(1, count):
+            print("{}: {}".format(i, sys.argv[i]))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+thats how we use import modules in python & how we work with which the file being script by (__name__)
